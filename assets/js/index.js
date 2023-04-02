1f4b5d4289b2da93ece38102b7d2346b5a171672
Roberto <Robertoaraujo230@hotmail.com>
2023-04-02 13:23:31 -0300

    Criando função para pegar os dados da weatherAPI

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,20 @@
+async function getWeatherInfo(location, apiMethod) {
+  let link;
+
+  if (apiMethod === "forecast.json")
+    link = `http://api.weatherapi.com/v1/forecast.json?key=8b0331a2ed8d4fefa7f214759230104&q=${location}&days=1&aqi=no&alerts=no&lang=pt`;
+  if (apiMethod === "current.json")
+    link = `https://api.weatherapi.com/v1/${apiMethod}?key=8b0331a2ed8d4fefa7f214759230104&q=${location}&aqi=no&lang=pt`;
+
+  try {
+    const weatherInfo = await fetch(link);
+    const weatherInfoJSON = await weatherInfo.json();
+    return weatherInfoJSON;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
 function getDayName(dayNumber) {
   let day;
   switch (dayNumber) {
