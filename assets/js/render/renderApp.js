fa8323539fdcd166b6a9d74047ca2314bb55fb2c
Roberto <Robertoaraujo230@hotmail.com>
2023-04-07 17:31:35 -0300

    Importando e iniciando a função renderWeekSection

--- assets/js/render/renderApp.js
+++ assets/js/render/renderApp.js
@@ -2,6 +2,7 @@ import renderAsideSection from "./renderAsideSection.js";
 import renderDateInfos from "./renderDateInfos.js";
 import renderOverviewSection from "./renderOverviewSection.js";
 import getWeatherInfo from "../utils/getWeatherInfo.js";
+import renderWeekSection from "../render/renderWeekSection.js";
 
 const renderApp = async (location) => {
   if (location === undefined) location = "Rio de Janeiro";
@@ -10,9 +11,12 @@ const renderApp = async (location) => {
     const weather = await getWeatherInfo(location, "forecast.json");
     const todayOverview = await weather.forecast.forecastday[0].day;
 
+    console.log(weather);
+
     renderDateInfos();
     await renderOverviewSection(todayOverview);
     await renderAsideSection(weather);
+    renderWeekSection(weather.forecast.forecastday);
   } catch (error) {
     console.log(error);
   }
