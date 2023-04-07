819b16432c4589b2c9f90a9771d1c798187b6c55
Roberto <Robertoaraujo230@hotmail.com>
2023-04-07 17:29:29 -0300

    Alterando o parâmetro para o forecast obter os dados dos próximos 7 dias

--- assets/js/utils/getWeatherInfo.js
+++ assets/js/utils/getWeatherInfo.js
@@ -2,7 +2,7 @@ async function getWeatherInfo(location, apiMethod) {
   let link;
 
   if (apiMethod === "forecast.json")
-    link = `http://api.weatherapi.com/v1/forecast.json?key=8b0331a2ed8d4fefa7f214759230104&q=${location}&days=1&aqi=no&alerts=no&lang=pt`;
+    link = `http://api.weatherapi.com/v1/forecast.json?key=8b0331a2ed8d4fefa7f214759230104&q=${location}&days=7&aqi=no&alerts=no&lang=pt`;
   if (apiMethod === "current.json")
     link = `https://api.weatherapi.com/v1/current.json?key=8b0331a2ed8d4fefa7f214759230104&q=${location}&aqi=no&lang=pt`;
 
