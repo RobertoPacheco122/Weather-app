38fece42c08a47ec96bcd1effc6d4f9814b813a4
Roberto <Robertoaraujo230@hotmail.com>
2023-04-08 17:11:29 -0300

    Levando para a função renderOverview a responsabilidade de criar o html da seção

--- assets/js/render/renderOverviewSection.js
+++ assets/js/render/renderOverviewSection.js
@@ -1,34 +1,88 @@
-function getOverviewElements() {
-  return {
-    minTemperature: document.querySelector("[data-min-temperature]"),
-    maxTemperature: document.querySelector("[data-max-temperature]"),
-    windSpeed: document.querySelector("[data-wind]"),
-    uvIndex: document.querySelector("[data-uv]"),
-    humidityCondition: document.querySelector("[data-humidity]"),
-    precipitation: document.querySelector("[data-precipitation]"),
-  };
-}
+const createOverviewHTML = ({
+  avghumidity,
+  uv,
+  totalprecip_mm,
+  maxwind_kph,
+  maxtemp_c,
+  mintemp_c,
+}) => {
+  const overviewSection = document.createElement("section");
+  overviewSection.classList.add("main__section", "main__section--overview");
 
-async function renderOverviewSection(todayOverview) {
-  const { avghumidity, uv, totalprecip_mm, maxwind_kph, maxtemp_c, mintemp_c } =
-    todayOverview;
+  overviewSection.innerHTML = `
+    <div class="main--container--today">
+      <h2 class="main__subtitle bold">Visão geral do dia</h2>
+      <ul class="main__list">
+        <li class="main__item">
+          <div class="main__item--image">
+            <i class="main__icon icon icon--min--temperature"></i>
+          </div>
+          <div class="main__item--content">
+            <p class="main__text main__text--itemTitle">Temperatura Mínima</p>
+            <p class="main__text main__text--info bold" data-min-temperature>${mintemp_c}° C</p>
+          </div>
+        </li>
+        <li class="main__item">
+          <div class="main__item--image">
+            <i class="main__icon icon icon--max--temperature"></i>
+          </div>
+          <div class="main__item--content">
+            <p class="main__text main__text--itemTitle">Temperatura Máxima</p>
+            <p class="main__text main__text--info bold" data-max-temperature>${maxtemp_c}° C</p>
+          </div>
+        </li>
+        <li class="main__item">
+          <div class="main__item--image">
+            <i class="main__icon icon icon--wind"></i>
+          </div>
+          <div class="main__item--content">
+            <p class="main__text main__text--itemTitle">Velocidade dos Ventos</p>
+            <p class="main__text main__text--info bold" data-wind>${maxwind_kph} km/h</p>
+          </div>
+        </li>
+        <li class="main__item">
+          <div class="main__item--image">
+            <i class="main__icon icon icon--sun"></i>
+          </div>
+          <div class="main__item--content">
+            <p class="main__text main__text--itemTitle">Índice de UV</p>
+            <p class="main__text main__text--info bold" data-uv>${uv}</p>
+          </div>
+        </li>
+        <li class="main__item">
+          <div class="main__item--image">
+            <i class="main__icon icon icon--humidity"></i>
+          </div>
+          <div class="main__item--content">
+            <p class="main__text main__text--itemTitle">Humidade do Ar</p>
+            <p class="main__text main__text--info bold" data-humidity>${avghumidity}%</p>
+          </div>
+        </li>
+        <li class="main__item">
+          <div class="main__item--image">
+            <i class="main__icon icon icon--precipitation"></i>
+          </div>
+          <div class="main__item--content">
+            <p class="main__text main__text--itemTitle">Total de Precipitação</p>
+            <p class="main__text main__text--info bold" data-precipitation>${totalprecip_mm} mm</p>
+          </div>
+        </li>
+      </ul>
+    </div>
+    <div class="main--container--week" data-week-container>
+      <h2 class="main__subtitle bold">Semana</h2>
+      <ul class="main__list--week" data-week-list>
+      </ul>
+    </div>
+  `;
 
-  const overviewElements = getOverviewElements();
-  const {
-    minTemperature,
-    maxTemperature,
-    windSpeed,
-    uvIndex,
-    humidityCondition,
-    precipitation,
-  } = overviewElements;
+  return overviewSection;
+};
 
-  minTemperature.innerText = `${mintemp_c}° C`;
-  maxTemperature.innerText = `${maxtemp_c}° C`;
-  windSpeed.innerText = `${maxwind_kph} km/h`;
-  uvIndex.innerText = `${uv}`;
-  humidityCondition.innerText = `${avghumidity}%`;
-  precipitation.innerText = `${totalprecip_mm} mm`;
-}
+const renderOverviewSection = (todayOverview) => {
+  const overviewSection = document.querySelector(".main--container--overview");
+  const overviewHTML = createOverviewHTML(todayOverview);
+  overviewSection.appendChild(overviewHTML);
+};
 
 export default renderOverviewSection;
