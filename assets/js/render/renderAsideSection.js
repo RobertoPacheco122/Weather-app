21bc39ac99f3cb87967509f859e753887aae2c55
Roberto <Robertoaraujo230@hotmail.com>
2023-04-08 17:10:50 -0300

    Levando para a função renderAside a responsabilidade de criar o html da seção

--- assets/js/render/renderAsideSection.js
+++ assets/js/render/renderAsideSection.js
@@ -1,37 +1,63 @@
-function getAsideElements() {
-  return {
-    cityLocation: document.querySelector("[data-location]"),
-    regionLocation: document.querySelector("[data-region]"),
-    weatherIcon: document.querySelector("[data-weather-icon]"),
-    weatherCondition: document.querySelector("[data-weather-condition]"),
-    degrees: document.querySelector("[data-degrees]"),
-    localTime: document.querySelector("[data-time]"),
-  };
-}
-
-function renderLocationWeather(
+const createAsideHTML = (
   { country, name, region, localtime },
-  { temp_c },
-  { text, icon }
-) {
-  const asideElements = getAsideElements();
+  { temp_c, condition: { text, icon } }
+) => {
+  const asideDiv = document.createElement("div");
+  asideDiv.classList.add("main--container--aside");
+  const time = localtime.replace(/\d{4}\-\d{2}\-\d{2} /, "");
 
-  const {
-    cityLocation,
-    regionLocation,
-    localTime,
-    weatherIcon,
-    weatherCondition,
-    degrees,
-  } = asideElements;
+  asideDiv.innerHTML = `
+    <header class="main__header--infos">
+      <div class="main--container--locationData">
+        <p class="main__text main__text--location bold" data-location>${name}</p>
+        <p class="main__text grey--text" data-region>${region}, ${country}</p>
+      </div>
+      <div class="main--container--locationHour">
+        <p class="main__text bold" data-time>${time}</p>
+      </div>
+    </header>
+    <section class="main__section main__section--weather">
+      <i class="main__icon main__icon--weather icon" data-weather-icon style="background-image: url(${icon})"></i>
+      <div class="main--container--weatherData">
+        <p class="main__text main__text--degrees bold" data-degrees>${temp_c}° C</p>
+        <p class="main__text grey--text" data-weather-condition>${text}</p>
+      </div>
+    </section>
+    <section class="main__section main__section--rain" data-rain>
+      <p class="main__subtitle bold">Chance de chuva</p>
+    </section>
+    <section class="main__section main__section--sun">
+      <p class="main__subtitle bold">Nascer e Pôr do Sol</p>
+      <ul class="main__list main__list--sun">
+        <li class="main__item main__item--sun">
+          <div class="main__item--image">
+            <i class="main__icon icon icon--sunrise"></i>
+          </div>
+          <div class="main__item--content">
+            <p class="main__text main__text--itemTitle grey--text">Nascer do Sol</p>
+            <p class="main__text main__text--info" data-time-sunrise></p>
+          </div>
+        </li>
+        <li class="main__item main__item--sun">
+          <div class="main__item--image">
+            <i class="main__icon icon icon--sunset"></i>
+          </div>
+          <div class="main__item--content">
+            <p class="main__text main__text--itemTitle grey--text">Pôr do Sol</p>
+            <p class="main__text main__text--info" data-time-sunset></p>
+          </div>
+        </li>
+      </ul>
+    </section>
+    <section class="main__section main__section--credits" style="display: flex;">
+      <p class="main__text">Powered by WeatherAPI.com</p>
+      
+      <a href="https://www.weatherapi.com/" title="Free Weather API"><img src='//cdn.weatherapi.com/v4/images/weatherapi_logo.png' alt="Weather data by WeatherAPI.com" border="0"></a>
+    </section>
+  `;
 
-  cityLocation.innerText = name;
-  regionLocation.innerText = `${region}, ${country}`;
-  localTime.innerText = localtime.replace(/\d{4}\-\d{2}\-\d{2} /g, "");
-  weatherIcon.style.backgroundImage = `url(${icon})`;
-  weatherCondition.innerText = text;
-  degrees.innerText = `${temp_c}° C`;
-}
+  return asideDiv;
+};
 
 function renderRainExpectation(forecast, hourNow) {
   const rainSection = document.querySelector("[data-rain]");
@@ -65,12 +91,14 @@ function renderSunsetAndRise(forecast) {
   sunsetElement.innerText = sunset;
 }
 
-async function renderAsideSection(weather) {
+function renderAsideSection(weather) {
   try {
+    const asideContainer = document.querySelector(".main--container--infos");
     const hourNow = new Date().getHours();
     const { location, current, forecast } = weather;
 
-    renderLocationWeather(location, current, current.condition);
+    const asideDivContent = createAsideHTML(location, current);
+    asideContainer.appendChild(asideDivContent);
     renderRainExpectation(forecast, hourNow);
     renderSunsetAndRise(forecast);
   } catch (error) {
